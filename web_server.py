f6d09cf26b8ec3eee1333eefbde16135f57d815f
runzexiaa <runze.xia@daocloud.io>
2017-09-26 16:46:36 +0800

    LOG & status code

--- web_server.py
+++ web_server.py
@@ -16,10 +16,12 @@ LOG = logging.getLogger()
 class MyDumpHandler(tornado.web.RequestHandler):
     def post(self):
         post_body = self.request.body.decode("utf-8")
+        LOG.info("get request %s", post_body)
         post_dict = json.loads(post_body)
         post_dict['event']['timestamp'] = self._convert_timezone(post_dict['event']['timestamp'])
         jira_dict = {'data': post_dict}
         request = BaseJiraAddress + self.request.uri
+        self.set_status(http.HTTPStatus.OK)
         self._execute_px_post_request(jira_dict, request)
         self.finish()
 
@@ -36,8 +38,10 @@ class MyDumpHandler(tornado.web.RequestHandler):
         r = requests.post(
             request,
             data=json.dumps(params), headers=headers)
+        LOG.info("get response %s", r)
         if r.status_code != http.HTTPStatus.OK:
             LOG.error("request jira error %s", r)
+            self.set_status(r.status_code,r)
         r.close()
 
 
